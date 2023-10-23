e2386d6943ee632f1cdc5c4512b5fc1c5abb25c7
Mechanic <dumashieanthony@gmail.com>
2023-10-23 13:29:01 +0100

    installed laravel and breeze

---
+++ database/migrations/2023_10_23_113831_create_personnel_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personnel_data', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('service_number');
+            $table->string('rank');
+            $table->string('surname');
+            $table->string('first_name');
+            $table->string('other_name')->nullable();
+            $table->date('date_of_birth');
+            $table->string('platoon');
+            $table->string('company');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personnel_data');
+    }
+};
---
+++ database/migrations/2023_10_23_113933_create_medical_records_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medical_records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('presenting_complaint');
+            $table->string('history_presenting_complaint');
+            $table->string('on_direct_questioning');
+            $table->string('past_medical_history');
+            $table->string('drug_history');
+            $table->string('family_history');
+            $table->string('social_history');
+            $table->string('on_examination');
+            $table->string('cardiovascular_system');
+            $table->string('respiratory_system');
+            $table->string('abdominal_system');
+            $table->string('central_nervous_system');
+            $table->string('musculoskeletal_system');
+            $table->string('diagnosis');
+            $table->string('treatment');
+            $table->string('referral');
+            $table->string('excuse_physical_training');
+            $table->string('excuse_duty');
+            $table->string('excuse_riffle_drill');
+            $table->string('excuse_boot');
+            $table->string('excuse_shaving');
+            $table->string('summary');
+            $table->string('service_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medical_records');
+    }
+};
